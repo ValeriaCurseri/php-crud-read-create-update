c768e736bfbcb7a6c666e168078d6a2bfbd542f9
Valeria Curseri <curserivaleria@gmail.com>
2020-10-01 16:31:17 +0200

    UPDATE con funzioni

--- partials/update/server-edit.php
+++ partials/update/server-edit.php
@@ -11,6 +11,7 @@
     $stmt = $conn->prepare($sql);
     $stmt->bind_param('iiii', $roomNumber, $floor, $beds, $id);
     $stmt->execute();
+    var_dump($stmt);
 
     if ($stmt && $stmt->affected_rows > 0) {        // se lo statemente è stato eseguito e la modifica è stata fatta
         header("Location: $basepath/view.php?id=$id");  // rimando l'utente alla view della stanza modificata
